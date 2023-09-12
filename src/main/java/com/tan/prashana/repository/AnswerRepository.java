afbe21971878234fb4fa38e1e8aefe81e1bc59a6
tanis <tanis@tanish29>
2023-09-12 18:08:53 +0530

    created repo for Answer ...

---
+++ src/main/java/com/tan/prashana/repository/AnswerRepository.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.tan.prashana.repository;
+
+import com.tan.prashana.dao.Answer;
+import com.tan.prashana.dao.Question;
+import java.util.Set;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ *
+ * @author tanish
+ */
+public interface AnswerRepository extends CrudRepository<Answer, Long> {
+    Set<Answer> findByQuestion(Question question);
+}
