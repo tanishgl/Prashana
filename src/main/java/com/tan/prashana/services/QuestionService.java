ffd0e7a9363d39aac7bc5b62f642c833903bacf4
tanis <tanis@tanish29>
2023-09-14 17:48:19 +0530

    adding options to question...

--- src/main/java/com/tan/prashana/services/QuestionService.java
+++ src/main/java/com/tan/prashana/services/QuestionService.java
@@ -4,10 +4,14 @@
  */
 package com.tan.prashana.services;
 
+import com.tan.prashana.dao.Answer;
 import com.tan.prashana.dao.Question;
 import com.tan.prashana.dao.Quiz;
 import com.tan.prashana.model.QuestionModel;
+import com.tan.prashana.repository.AnswerRepository;
 import com.tan.prashana.repository.QuestionRepository;
+import java.util.HashSet;
+import java.util.Set;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -19,12 +23,20 @@ import org.springframework.stereotype.Service;
 public class QuestionService {
     
     @Autowired private QuestionRepository questionRepository;
+    @Autowired private AnswerRepository answerRepository;
+    @Autowired private AnswerService answerService;
     
     public Question createQuestion(Quiz quiz, QuestionModel questionModel){
         Question ques = new Question();
         ques.setQuiz(quiz);
         ques.setQuestionDesc(questionModel.getQuestionDesc());
         ques.setCorrectAnswer(questionModel.getCorrectAnswer());
+        Set<String> options = questionModel.getOptions();
+        Set<Answer> questionOptions = new HashSet<>();
+        for(String answer : options){
+            Answer option = answerService.createAnswer(answer);
+            questionOptions.add(option);
+        }
         questionRepository.save(ques);
         quiz.getQuestionSet().add(ques);
         return ques;
